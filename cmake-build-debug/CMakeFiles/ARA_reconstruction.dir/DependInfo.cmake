b711845bc32b01d7701ba70e5c9bfb16e7242012
Rishabh Khandelwal <rkhandelwal@dyn-13-228.icecube.wisc.edu>
2018-03-28 15:30:52 -0500

    adding the setup file for spicecore

---
+++ cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/kernel_3D_analysis.c" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/kernel_3D_analysis.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/Healpix_Onion.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/Healpix_Onion.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/analysis.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/analysis.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/analysis_2015DeepPulser.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/analysis_2015DeepPulser.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/analysis_2017DeepPulser.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/analysis_2017DeepPulser.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/analysis_angle.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/analysis_angle.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/analysis_anotherPol.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/analysis_anotherPol.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/analysis_constantNFilter.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/analysis_constantNFilter.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/analysis_treg.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/analysis_treg.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/calibrationTools.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/calibrationTools.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/calibrationToolsVs3.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/calibrationToolsVs3.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/evProcessTools.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/evProcessTools.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/recoData.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/recoData.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/recoSettings.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/recoSettings.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/recoTools.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/recoTools.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/trackEngine.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/trackEngine.cxx.o"
+  "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/treg.cxx" "/Users/rkhandelwal/Rishabh/software/repo/ARA_reconstruction/cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/treg.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ARA_reconstruction.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ARA_reconstruction.dir/analysis.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/analysis_2015DeepPulser.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/analysis_2017DeepPulser.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/analysis_angle.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/analysis_anotherPol.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/analysis_constantNFilter.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/analysis_treg.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/calibrationTools.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/calibrationToolsVs3.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/evProcessTools.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/Healpix_Onion.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/kernel_3D_analysis.c.o"
+  "CMakeFiles/ARA_reconstruction.dir/recoData.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/recoSettings.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/recoTools.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/trackEngine.cxx.o"
+  "CMakeFiles/ARA_reconstruction.dir/treg.cxx.o"
+  "ARA_reconstruction.pdb"
+  "ARA_reconstruction"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/ARA_reconstruction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
